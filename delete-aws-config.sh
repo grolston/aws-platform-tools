198ca56465f0655da875db619584ab0498807ed1
George Rolston <grolston@users.noreply.github.com>
2024-01-15 21:55:23 -0700

    Update delete-aws-config.sh

--- delete-aws-config.sh
+++ delete-aws-config.sh
@@ -3,7 +3,7 @@ regions=$(aws ec2 describe-regions --query 'Regions[*].RegionName' --output text
 for region in $regions; do
     echo "Processing region: $region"
     recorder_exists=$(aws configservice describe-configuration-recorders --region $region --query 'ConfigurationRecorders | length')
-    if [ "$recorder_exists" -gt 0 ]; then
+    if [ "$recorder_exists" -gt 0 ] || [ -n "$recorder_exists" ]; then
         echo "Deleting AWS Config recorder in $region..."
         aws configservice delete-configuration-recorder --region $region
         echo "AWS Config recorder deleted in $region."
@@ -11,7 +11,7 @@ for region in $regions; do
         echo "No AWS Config recorder found in $region."
     fi
     channel_exists=$(aws configservice describe-delivery-channels --region $region --query 'DeliveryChannels | length')
-    if [ "$channel_exists" -gt 0 ]; then
+    if [ "$channel_exists" -gt 0 ] || [ -n "$channel_exists" ]; ; then
         echo "Deleting AWS Config delivery channel in $region..."
         aws configservice delete-delivery-channel --region $region
         echo "AWS Config delivery channel deleted in $region."
